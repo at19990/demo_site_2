b48f0c6bcb83fb988967e1f89380e4f22e432a7b
at19990 <a.tsuda19990@gmail.com>
2020-08-19 13:45:55 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,29 @@ export default () => (
     </h2>
     <br />
     <footer className="footer">
+    <div style={{ maxWidth: '100vw' }} className="columns">
+              <div className="column is-4">
+                <section className="menu">
+                  <ul className="menu-list">
+                    <li>
+                      <Link to="/" className="navbar-item">
+                        ホーム
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/about">
+                        当サイトについて
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/blog">
+                        記事一覧
+                      </Link>
+                    </li>
+                    
+                    
+                  </ul>
+                </section>
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
