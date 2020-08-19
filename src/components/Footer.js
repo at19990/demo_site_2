567c82061f695e09b9f3d5c28ea62954940ac92c
at19990 <a.tsuda19990@gmail.com>
2020-08-19 14:16:25 +0900

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Footer.css'
 import { Link } from 'gatsby'
 
+import facebook from '../../images/social/facebook.svg'
+import instagram from '../../images/social/instagram.svg'
+import twitter from '../../images/social/twitter.svg'
+import vimeo from '../../images/social/vimeo.svg'
+
 export default () => (
   <div>
     <h2 className="taCenter">
@@ -35,6 +40,60 @@ export default () => (
                 </section>
             </div>
              
+            <div className="column is-4">
+                <section>
+                  <ul className="menu-list">
+                    
+                    <li>
+                      <Link className="navbar-item" to="/contact">
+                        お問い合わせ
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        className="navbar-item"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        管理者用ページ
+                      </a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+    
+            <div className="column is-4 social">
+                <a title="facebook" href="https://facebook.com">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="instagram" href="https://instagram.com">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="vimeo" href="https://vimeo.com">
+                  <img
+                    src={vimeo}
+                    alt="Vimeo"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+              </div>
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
